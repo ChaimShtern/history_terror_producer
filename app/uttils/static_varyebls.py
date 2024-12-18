a90864f584d5acaf2bcc937de6896bf11c0b6403
Chimkes <chaimlogan31@gmail.com>
2024-12-18 11:51:46 +0200

    create utils and split data

---
+++ app/uttils/static_varyebls.py
@@ -0,0 +1,30 @@
+relevant_fields = [
+    # זמנים ותאריכים
+    'iyear', 'imonth', 'iday',
+
+    # מיקום גיאוגרפי
+    'latitude', 'longitude', 'country_txt', 'region_txt', 'city',
+
+    # מידע על קבוצות תוקפות
+    'gname', 'guncertain1', 'nperps',
+
+    # מאפייני התקיפה
+    'attacktype1_txt', 'attacktype2_txt', 'attacktype3_txt',
+    'targtype1_txt', 'target1', 'natlty1_txt',
+
+    # נתוני נשק
+    'weaptype1_txt', 'weapsubtype1_txt',
+    'weaptype2_txt', 'weapsubtype2_txt',
+    'weaptype3_txt', 'weapsubtype3_txt',
+    'weaptype4_txt', 'weapsubtype4_txt',
+
+    # נפגעים ונזק
+    'nkill', 'nwound', 'nkillus', 'nwoundus',
+    'property', 'propvalue', 'propextent_txt',
+
+    # חטיפות וסחיטה
+    'ishostkid', 'nhostkid', 'ransom', 'ransomamt', 'hostkidoutcome_txt',
+
+    # הערות ונתוני מקור
+    'summary', 'addnotes', 'scite1', 'scite2', 'scite3'
+]
